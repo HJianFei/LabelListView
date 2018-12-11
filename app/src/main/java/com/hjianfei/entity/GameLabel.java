e48b86e46a04f2842bbec1390d92aba77ea16a34
HJianFei <hjf@jintuoguan.com>
2018-12-11 15:19:25 +0800

    init

---
+++ app/src/main/java/com/hjianfei/entity/GameLabel.java
@@ -0,0 +1,11 @@
+package com.hjianfei.entity;
+
+/**
+ * @author Linhai GU
+ */
+public class GameLabel {
+    public String name;
+    public String textColor;// 字体颜色
+    public String backgroudColor;// 标签背景颜色
+    public String strokeColor;// 标签外框颜色
+}
---
+++ app/src/main/java/com/hjianfei/inface/IOnItemClickListener.java
@@ -0,0 +1,12 @@
+package com.hjianfei.inface;
+
+/**
+ * 用于点击标签回调接口
+ *
+ * @author Linhai Gu
+ */
+public interface IOnItemClickListener {
+
+    public void onClick(String name, int position);
+
+}
